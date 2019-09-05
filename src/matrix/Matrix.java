122b273c702853f58276aa357bdcb073baef637b
Jason Fujii <Jasonfujii@yahoo.com>
2019-09-04 23:52:38 -0700

    added more to the methods

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -4,22 +4,31 @@
  * and open the template in the editor.
  */
 package matrix;
-
+import Java.util.*;
 /**
  *
  * @author jasonfujii
  */
-public class Matrix {
+public class Matrix implements matrixInterface {
 
     private int[][] mat;
     private int row;
     private int col;
+    private boolean isIdentity;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Matrix m = new Matrix(3,3);
+        m.fill();
+        System.out.println(m.printMatrix());
+        Matrix m1 = new Matrix(3,3);
+        m1.fill();
+        
+        Matrix m2 = m.add(m1);
+        System.out.println(m2.printMatrix());
     }
     
     public Matrix()
@@ -33,6 +42,106 @@ public class Matrix {
         mat = new int[r][c];
         row = r;
         col = c;
+        //fill();
     }
     
+    public int getVal(int r, int c)
+    {
+        return mat[r][c];
+    }
+    
+    public void setVal(int r, int c, int val)
+    {
+        mat[r][c] = val;
+    }
+    
+    public void fill()
+    {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        for(int r = 0; r < row; r++)
+        {
+            for(int c = 0; c < col; c++)
+            {
+                System.out.println("Please insert the value for [" +r+ "][" +c+ "]");
+                mat[r][c] = sc.nextInt();
+            }
+        }
+    }
+    
+    public Matrix add(Matrix m)
+    {
+        Matrix sum = new Matrix(row, col);
+        if(row == m.getRow() && col == m.getCol())
+        {
+            for(int r = 0; r < row; r++)
+            {
+                for(int c = 0; c < col; c++)
+                {
+                    sum.setVal(r,c, (mat[r][c] + m.getVal(r,c)));
+                }
+            }
+        }
+        else
+            System.out.println("You cannot add matrices of different size");
+        
+        return sum;
+    }
+    
+    public Matrix subtract(Matrix m)
+    {   
+        Matrix diff = new Matrix(row, col);
+        
+        return diff;
+    }
+    
+    public Matrix mult(Matrix m)
+    {
+        Matrix product = new Matrix(col, m.getRow());
+        
+        return product;
+    }
+    
+    public Matrix div(Matrix m)
+    {
+        Matrix quotient = new Matrix(col, m.getRow());
+        
+        return quotient;
+    }
+    
+    public String printMatrix()
+    {
+        String matrix = new String("");
+        for(int r = 0; r < row; r++)
+        {
+            for(int c = 0; c < col; c++)
+            {
+                matrix = matrix + mat[r][c] + " ";
+            }
+            matrix = matrix + "\n";
+        }
+        return matrix;
+    }
+    
+    public String printSystem()
+    {
+        return " ";
+    }
+    
+    public boolean isEqual()
+    {
+        return false;
+    }    
+    
+    public int getCol()
+    {
+        return col;
+    }
+    
+    public int getRow()
+    {
+        return row;
+    }
+    
+    
+
 }
--- src/matrix/matrixInterface.java
+++ src/matrix/matrixInterface.java
@@ -11,11 +11,37 @@ package matrix;
  */
 public interface matrixInterface {
     //fill() has the user input the matrix values into the matrix
-    public void fill();
+    //Will be private
+    void fill();
     
-    //Add(): adds two matrices together. *Matrices MUST be the same size*
-    public void add(Matrix m);
+    //Add(): adds two matrices together and returns a new matrix with the added values. 
+    //*Matrices MUST be the same size*
+    public Matrix add(Matrix m);
     
-    //Subtract(): subtrscts 
+    //Subtract(): subtracts two matrices from eachother and returns a new matrix with the subtracted values 
+    //*Matrices MUST be the same size* 
+    public Matrix subtract(Matrix m);
+    
+    //Mult(): multiplies two matrices together and returns a new matrix with the multiplied values.
+    //*Matrix inside dimensions must be the same (3x3 & 3x5)*
+    public Matrix mult(Matrix m);
+    
+    //Div(): divides two matrices and returns a new matrix with the divided values. 
+    //*Matrices' inside dimensions must be the same (3x3 & 3x5)*
+    public Matrix div(Matrix m);
+    
+    //printMatrix(): prints out the Matrix in Augmented Matrix form
+    public String printMatrix();
+    
+    //printSystem(): prints out the Matrix in system of equation form
+    public String printSystem();
+    
+    //isEqual(): returns true if every element in two matrices are the same.
+    //returns false if matrices are not the same size or if they are not identical
+    public boolean isEqual();
+    
+    //isIdentity(): returns true if the matrix is an identity matrix (every element except for those in the diagonal of the matrix are 0. 
+    //The diagonals are 1) returns false if the Matrix is not an identity matrix
+    
 }
     
